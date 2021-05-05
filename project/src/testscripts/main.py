7c4a19e9bc93f23dd6256bf8cf293902de603377
yurii <yuralrock@gmail.com>
2021-05-05 14:36:20 +0300

    add testcase RemoteIPBelongsToProxy

--- project/src/testscripts/main.py
+++ project/src/testscripts/main.py
@@ -48,6 +48,45 @@ class WebPageOpensInChrome(aetest.Testcase):
         self.grid.stop()
 
 
+class RemoteIPBelongsToProxy(aetest.Testcase):
+    @aetest.setup
+    def start_services(self, testbed):
+        self.proxy_device = testbed.devices["proxy-vm"]
+        self.proxy_connection = Proxy(self.proxy_device)
+        self.proxy_connection.start()
+
+        self.user_device = testbed.devices["user-1"]
+        self.grid = SeleniumGrid(self.user_device)
+        self.grid.start()
+
+    @aetest.test
+    def get_remote_ip(self):
+        proxy_net_ifs = self.proxy_device.interfaces.names.pop()
+        proxy_ip = self.proxy_device.interfaces[proxy_net_ifs].ipv4.ip.compressed
+
+        with Chrome(self.user_device) as chrome:
+            chrome.open(
+                host="https://wiki.archlinux.org/",
+                proxy_host=self.proxy_device,
+                write_pcap=False,
+                timeout=30,
+            )
+            stats = chrome.get_stats("response.json")
+        serialized_stats = serializer(stats)
+        data = BrowserResponseAnalyzer(serialized_stats)
+        remote_ip = data.get_remote_ip_port()[0]
+
+        if remote_ip != proxy_ip:
+            self.failed(
+                f"Invalid remote address, expected {proxy_ip}, got {remote_ip}!"
+            )
+
+    @aetest.cleanup
+    def stop_services(self):
+        self.proxy_connection.stop()
+        self.grid.stop()
+
+
 class CommonCleanup(aetest.CommonCleanup):
     pass
 
