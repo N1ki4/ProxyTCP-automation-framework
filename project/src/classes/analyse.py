b46152cf79a847c32c4bbf073f50ac17c21864f6
N1ki4 <glazunov_n@ukr.net>
2021-05-06 15:17:04 +0300

    Fixed linter issues
    Fixed flake8 blank lines contains whitespace

--- project/src/classes/analyse.py
+++ project/src/classes/analyse.py
@@ -110,11 +110,11 @@ class BrowserResponseAnalyzer:
         else:
             result.append(self._critical_error)
         return result
-    
+
     def get_requests_statistics(self) -> tuple:
         request_sent_pattern = "\\bNetwork.requestWillBeSent\\b"
         return len([*re.finditer(request_sent_pattern, str(self._response))])
-    
+
     def get_response_statistics(self) -> tuple:
         response_recieved_pattern = "\\bNetwork.responseReceived\\b"
         return len([*re.finditer(response_recieved_pattern, str(self._response))])
