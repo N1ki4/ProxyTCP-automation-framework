7451558c7326af956c4975e6eb778e9385bdcac4
yurii <yuralrock@gmail.com>
2021-05-25 18:53:00 +0300

    remove failing test from smoke suite

--- project/src/testscripts/smoke.py
+++ project/src/testscripts/smoke.py
@@ -1,14 +1,10 @@
 # pylint: disable=no-self-use # pyATS-related exclusion
 # pylint: disable=attribute-defined-outside-init # pyATS-related exclusion
-import os
 import logging
 from pprint import pformat
 
-
 from pyats import aetest
 
-
-import src
 from src.classes.remote_tools import SeleniumGrid
 from src.classes.clients import Chrome, Curl
 from src.classes.analyse import BrowserResponseAnalyzer
@@ -19,32 +15,28 @@ _log = logging.getLogger(__name__)
 
 class CommonSetup(aetest.CommonSetup):
     @aetest.subsection
-    def update_testscript_parameters(self, testbed, unicon_logfile):
+    def update_testscript_parameters(self, testbed):
         user_device = testbed.devices["user-2"]
         proxy_device = testbed.devices["proxy-vm"]
-        unicon_logfile = os.path.join(src.__path__[0], "logs", unicon_logfile)
 
         self.parent.parameters.update(
             {
                 "user": user_device,
                 "proxy": proxy_device,
-                "unicon_logfile": unicon_logfile,
             }
         )
 
     @aetest.subsection
-    def start_selenium(self, user, unicon_logfile):
-        grid = SeleniumGrid(user, unicon_logfile)
+    def start_selenium(self, user):
+        grid = SeleniumGrid(user)
         grid.start()
 
 
 class WebPageOpensInChrome(aetest.Testcase):
     @aetest.test
-    def test_page_open(self, user, proxy, host, unicon_logfile):
+    def test_page_open(self, user, proxy, host):
 
-        with Chrome(
-            grid_server=user, proxy_server=proxy, unicon_log=unicon_logfile
-        ) as chrome:
+        with Chrome(grid_server=user, proxy_server=proxy) as chrome:
             chrome.get(host)
             stats = chrome.get_stats()
 
@@ -59,13 +51,11 @@ class WebPageOpensInChrome(aetest.Testcase):
 
 class RemoteIPBelongsToProxy(aetest.Testcase):
     @aetest.test
-    def get_remote_ip(self, user, proxy, host, unicon_logfile):
+    def get_remote_ip(self, user, proxy, host):
         proxy_net_ifs = proxy.interfaces.names.pop()
         proxy_ip = proxy.interfaces[proxy_net_ifs].ipv4.ip.compressed
 
-        with Chrome(
-            grid_server=user, proxy_server=proxy, unicon_log=unicon_logfile
-        ) as chrome:
+        with Chrome(grid_server=user, proxy_server=proxy) as chrome:
             chrome.get(host)
             stats = chrome.get_stats()
 
@@ -78,73 +68,15 @@ class RemoteIPBelongsToProxy(aetest.Testcase):
             )
 
 
-class ProxyDoesNotAlterPorts(aetest.Testcase):
-    @aetest.setup
-    def setup_loops(self):
-        aetest.loop.mark(self.no_error_test, host=self.parameters["no_error"])
-        aetest.loop.mark(self.ssl_error_test, host=self.parameters["ssl_error"])
-        aetest.loop.mark(self.con_error_test, host=self.parameters["connection_error"])
-
-    @aetest.test
-    def no_error_test(self, user, proxy, host, unicon_logfile):
-
-        with Chrome(
-            grid_server=user, proxy_server=proxy, unicon_log=unicon_logfile
-        ) as chrome:
-            chrome.get(host)
-            stats = chrome.get_stats()
-
-        data = BrowserResponseAnalyzer(stats)
-        status_code = data.get_status_code()
-        if status_code != 200:
-            _log.info(f"Web Brower logs:\n{pformat(stats)}")
-            self.failed(
-                f"Invalid response, expected status code 200, got {status_code}!"
-            )
-
-    @aetest.test
-    def ssl_error_test(self, user, proxy, host, unicon_logfile):
-
-        with Chrome(
-            grid_server=user, proxy_server=proxy, unicon_log=unicon_logfile
-        ) as chrome:
-            chrome.get(host)
-            stats = chrome.get_stats()
-
-        data = BrowserResponseAnalyzer(stats)
-        errors = data.get_browser_errors()
-        pass_condition = len(errors) == 1 and "ERR_SSL_PROTOCOL_ERROR" in errors[0]
-        if not pass_condition:
-            _log.info(f"Web Brower logs:\n{pformat(stats)}")
-            self.failed("Invalod response, no `ERR_SSL_PROTOCOL_ERROR` occured!")
-
-    @aetest.test
-    def con_error_test(self, user, proxy, host, unicon_logfile):
-
-        with Chrome(
-            grid_server=user, proxy_server=proxy, unicon_log=unicon_logfile
-        ) as chrome:
-            chrome.get(host)
-            stats = chrome.get_stats()
-
-        data = BrowserResponseAnalyzer(stats)
-        errors = data.get_browser_errors()
-        pass_condition = len(errors) == 1 and "ERR_CONNECTION_REFUSED" in errors[0]
-        if not pass_condition:
-            _log.info(f"Web Brower logs:\n{pformat(stats)}")
-            self.failed("Invalod response, no `ERR_CONNECTION_REFUSED` occured!")
-
-
 class InvalidProxyHost(aetest.Testcase):
     @aetest.test
-    def connect_invalid_proxy_ip(self, user, proxy, host, proxy_ip, unicon_logfile):
+    def connect_invalid_proxy_ip(self, user, proxy, host, proxy_ip):
 
         with Curl(
             client_server=user,
             proxy_server=proxy,
             session_timeout=10,
             proxy_ip=proxy_ip,
-            unicon_log=unicon_logfile,
         ) as curl:
             curl.get(host)
             stats = curl.get_response()
@@ -155,14 +87,13 @@ class InvalidProxyHost(aetest.Testcase):
 
 class InvalidProxyPort(aetest.Testcase):
     @aetest.test
-    def connect_invalid_proxy_port(self, user, proxy, host, proxy_port, unicon_logfile):
+    def connect_invalid_proxy_port(self, user, proxy, host, proxy_port):
 
         with Curl(
             client_server=user,
             proxy_server=proxy,
             session_timeout=10,
             proxy_port=proxy_port,
-            unicon_log=unicon_logfile,
         ) as curl:
             curl.get(host)
             stats = curl.get_response()
