2da784726b750a41e20bc8c1ff7a344976503ccf
yurii <yuralrock@gmail.com>
2021-05-31 14:37:09 +0300

    edit jobs

--- project/src/jobs/build_environment.py
+++ project/src/jobs/build_environment.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 import logging
+from time import sleep
 
 from pyats.easypy import run
 
@@ -11,7 +12,7 @@ _scripts_dir = os.path.join(src.__path__[0], "envscripts")
 
 
 def main(runtime):
-    max_reps = 2
+    max_reps = 3
 
     logging.getLogger().setLevel(logging.INFO)
     logging.getLogger("src.environment.google_cloud_setup.builder").setLevel(
@@ -23,21 +24,15 @@ def main(runtime):
     parser.add_argument("--service-key", dest="service_key")
     args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
 
-    tasks = ("environment_setup.py", "systemundertest_setup.py")
+    testscript = os.path.join(_scripts_dir, "environment_setup.py")
 
-    for script in tasks:
-        task_success = True
-        testscript = os.path.join(_scripts_dir, script)
-        for i in range(max_reps):
-            result = run(
-                runtime=runtime, testscript=testscript, service_key=args.service_key
-            )
-            if result:
-                break
-            if i != max_reps - 1:
-                _log.info("Job failed, repeating attempt!")
-            else:
-                task_success = False
-                _log.error("Job failed!")
-        if not task_success:
+    for i in range(max_reps):
+        result = run(
+            runtime=runtime, testscript=testscript, service_key=args.service_key
+        )
+        if result:
             break
+        if i != max_reps - 1:
+            _log.info("Job failed, repeating attempt!")
+        else:
+            _log.error("Job failed!")
--- project/src/jobs/destroy_environment.py
+++ project/src/jobs/destroy_environment.py
@@ -12,15 +12,27 @@ _scripts_dir = os.path.join(src.__path__[0], "envscripts")
 
 
 def main(runtime):
+    max_reps = 2
 
     logging.getLogger().setLevel(logging.INFO)
     logging.getLogger("src.environment.google_cloud_setup.builder").setLevel(
         logging.INFO
     )
+    _log = logging.getLogger(__name__)
 
     parser = argparse.ArgumentParser(description="standalone parser")
     parser.add_argument("--service-key", dest="service_key")
     args, sys.argv[1:] = parser.parse_known_args(sys.argv[1:])
 
     testscript = os.path.join(_scripts_dir, "environment_cleanup.py")
-    run(runtime=runtime, testscript=testscript, service_key=args.service_key)
+
+    for i in range(max_reps):
+        result = run(
+            runtime=runtime, testscript=testscript, service_key=args.service_key
+        )
+        if result:
+            break
+        if i != max_reps - 1:
+            _log.info("Job failed, repeating attempt!")
+        else:
+            _log.error("Job failed!")
